4f6f0d405948af2808de843859f9027d9908fd65
achchuproject <archanakshetty1983@gmail.com>
2024-08-27 15:57:52 +0530

    project shared

---
+++ AKSSelenium/src/test/java/pac2/orgTest.java
@@ -0,0 +1,16 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("execute createorgTest");
+	}
+	@Test
+	public void modifyOrgTest()
+	{
+		System.out.println("execute modifyorgTest");
+	}
+}
